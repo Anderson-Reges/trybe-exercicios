53e8c973b76ef9b7f0501bbc7b8210faa9cc1f4a
Anderson-Reges <andersonreges76@gmail.com>
2022-06-29 18:29:22 -0300

    add fixação do conteudo buscando elementos

---
+++ Fundamentos/bloco-05-JavaScript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/conteudos/buscando-elementos/script2.js
@@ -0,0 +1,18 @@
+//1
+
+const acess = document.getElementById('elementoOndeVoceEsta')
+console.log(acess);
+
+//2
+
+const changeColor = document.getElementById('elementoOndeVoceEsta').parentElement
+changeColor.style.color = 'red';
+
+//3
+
+const addText = document.getElementById('primeiroFilhoDoFilho').innerText = 'Hello';
+
+//4
+
+const acess2 = pai.firstElementChild;
+console.log(acess2);
